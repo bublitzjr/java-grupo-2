213a3ae7e6aec5b747a531e759e728f9c09b173e
LorranPdS <lorransantospereira@yahoo.com.br>
2021-03-11 09:34:46 -0300

    corrigindo geração de id em sala e pessoas

--- src/main/java/com/gestaotreinamento/model/Pessoa.java
+++ src/main/java/com/gestaotreinamento/model/Pessoa.java
@@ -2,11 +2,7 @@ package com.gestaotreinamento.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Pessoa implements Serializable {
@@ -14,8 +10,7 @@ public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(generator = "gen_idpessoa", strategy = GenerationType.IDENTITY)
-	private Long id;
+	private int id;
 
 	@Column(nullable = false, length = 25)
 	private String nomePessoa;
@@ -28,12 +23,12 @@ public class Pessoa implements Serializable {
 
 	private String cafeEtapa1;
 	private String cafeEtapa2;
-	
-	public Long getId() {
+
+	public int getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
@@ -83,31 +78,6 @@ public class Pessoa implements Serializable {
 
 	public void setCafeEtapa2(String cafeEtapa2) {
 		this.cafeEtapa2 = cafeEtapa2;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pessoa other = (Pessoa) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 	@Override
--- src/main/java/com/gestaotreinamento/model/Sala.java
+++ src/main/java/com/gestaotreinamento/model/Sala.java
@@ -2,11 +2,7 @@ package com.gestaotreinamento.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Sala implements Serializable {
@@ -14,19 +10,18 @@ public class Sala implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(generator = "gen_id", strategy = GenerationType.IDENTITY)
-	private Long id;
+	private int id;
 
 	@Column(nullable = false, length = 25)
 	private String nomeSala;
 
-	private Byte lotacao = 20;
+	private Byte lotacao;
 
-	public Long getId() {
+	public int getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
@@ -44,31 +39,6 @@ public class Sala implements Serializable {
 
 	public void setLotacao(Byte lotacao) {
 		this.lotacao = lotacao;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Sala other = (Sala) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 	@Override
